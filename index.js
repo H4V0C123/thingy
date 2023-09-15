3f62533e56639847d36734373c4f7e72ff8b2119
H4V0C123 <134865302+H4V0C123@users.noreply.github.com>
2023-09-15 14:12:28 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("demo").innerHTML = "Hello JavaScript!";
+});
---
+++ quiz.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var q1 = document.getElementById("q1");
+    var q2 = document.getElementById("q2");
+    
+});
